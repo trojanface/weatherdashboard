1cabbd01c65a41ee858664bc3d5ab42dd2347b84
MattT <trojanface@hotmail.com>
2020-03-24 17:34:49 +1030

    Created a basic api call

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $("#searchBar").on("submit", function () {
     event.preventDefault();
 
-console.log($("#searchText").val());
+    console.log($("#searchText").val());
+    let cityName = $("#searchText").val();
+    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=2a41be6b56e8918bc7efe98c840f4638";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
 
+        });
 });
